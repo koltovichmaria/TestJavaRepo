9f205ca4bf24f36e562b43bad1d6ccc34ebfe5d6
aboba <aboba@mail.ru>
2022-11-11 17:54:11 +0300

    Create method
    test

--- TestJavaProject/src/Main.java
+++ TestJavaProject/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    public static void test(){
+        System.out.println("heheh");
+    }
     public static void main(String[] args) {
+        System.out.println("coco");
         System.out.println("Hello world!");
     }
 }
