bf6cc38023f61e276d9a0c9f5f91e0139426a713
aboba <aboba@mail.ru>
2022-11-11 17:56:04 +0300

    Revert "Create method"
    This reverts commit 9f205ca4bf24f36e562b43bad1d6ccc34ebfe5d6.

--- TestJavaProject/src/Main.java
+++ TestJavaProject/src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
-    public static void test(){
-        System.out.println("heheh");
-    }
     public static void main(String[] args) {
-        System.out.println("coco");
         System.out.println("Hello world!");
     }
 }
