c9f1d89e2f9e365dbf1d2492c20c2832a23a78e2
aboba <aboba@mail.ru>
2022-11-16 15:44:36 +0300

    enter

--- TestJavaProject/src/Main.java
+++ TestJavaProject/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        double a = in.nextDouble();
+        String znak = in.nextLine();
+        double b = in.nextDouble();
+
+
     }
 }
