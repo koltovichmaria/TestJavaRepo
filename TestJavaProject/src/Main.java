a774f460a16bb3d2b302f61656fc08ad063dc8fa
koltovichmaria <99329148+koltovichmaria@users.noreply.github.com>
2022-11-16 17:42:37 +0300

    examples

--- TestJavaProject/src/Main.java
+++ TestJavaProject/src/Main.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(1,2));
+        System.out.println(calc.divide(50,2));
+        System.out.println(calc.multiply(40, 40));
+        System.out.println(calc.substract(1,2));
     }
 }
