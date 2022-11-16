16d72e4be3be2339b77591702389c294a0320f57
koltovichmaria <99329148+koltovichmaria@users.noreply.github.com>
2022-11-16 17:32:41 +0300

    delete all in main

--- TestJavaProject/src/Main.java
+++ TestJavaProject/src/Main.java
@@ -3,15 +3,5 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("YOOOOOOOOOOOOOOOOOOOOOOOOOO");
-
-        Scanner in = new Scanner(System.in);
-        double a = in.nextDouble();
-        String operation = in.nextLine();
-        double b = in.nextDouble();
-
-        System.out.println("Hello gays");
-        System.out.println("Hello Sucks!");
     }
 }
